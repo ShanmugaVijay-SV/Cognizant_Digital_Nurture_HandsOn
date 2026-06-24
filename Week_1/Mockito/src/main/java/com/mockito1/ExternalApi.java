4048e7824c64547e905df1105d6a4e9374c78b23
ShanmugaVijay-SV <shanmugavijayt@gmail.com>
2026-06-24 09:48:55 +0530

    Mockito ex1

---
+++ Week_1/Mockito/src/main/java/com/mockito1/ExternalApi.java
@@ -0,0 +1,8 @@
+package com.mockito1;
+
+public class ExternalApi {
+
+    public String getData() {
+        return "Real Data";
+    }
+}
---
+++ Week_1/Mockito/src/test/java/com/mockito1/MyServiceTest.java
@@ -0,0 +1,29 @@
+package com.mockito1;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MyServiceTest {
+	@Test
+    void testExternalApi() {
+
+        // Arrange
+        ExternalApi mockApi =
+                mock(ExternalApi.class);
+
+        when(mockApi.getData())
+                .thenReturn("Mock Data");
+
+        MyService service =
+                new MyService(mockApi);
+
+        // Act
+        String result =
+                service.fetchData();
+
+        // Assert
+        assertEquals("Mock Data", result);
+    }
+}
